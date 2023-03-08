224f2f1a730fb77709ff9c395efd64319fe540bf
Samuel Bourassa <district67@gmail.com>
2023-03-08 17:42:53 -0500

    Add a key that stored the boolean value that specifies whether the layout used is linear.

--- app/src/main/java/com/example/wordsapp/data/SettingsDataStore.kt
+++ app/src/main/java/com/example/wordsapp/data/SettingsDataStore.kt
@@ -3,10 +3,13 @@ package com.example.wordsapp.data
 import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
 
 
-class SettingsDataStore(context: Context) {}
+class SettingsDataStore(context: Context) {
+    private val IS_LINEAR_LAYOUT_MANAGER = booleanPreferencesKey("is_linear_layout_manager")
+}
 
 private const val LAYOUT_PREFERENCES_NAME = "layout_preferences"
 private val Context.dataStore : DataStore<Preferences> by preferencesDataStore(
