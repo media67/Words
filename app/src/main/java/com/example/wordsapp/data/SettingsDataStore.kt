eb437f22d1488bdfe18969c44adf46e064d71110
Samuel Bourassa <district67@gmail.com>
2023-03-08 17:49:35 -0500

    Create saveLayoutToPreferencesDataStore suspend function to write to the Preferences DataStore.

--- app/src/main/java/com/example/wordsapp/data/SettingsDataStore.kt
+++ app/src/main/java/com/example/wordsapp/data/SettingsDataStore.kt
@@ -4,11 +4,20 @@ import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.preferencesDataStore
+import androidx.recyclerview.widget.LinearLayoutManager
 
 
 class SettingsDataStore(context: Context) {
     private val IS_LINEAR_LAYOUT_MANAGER = booleanPreferencesKey("is_linear_layout_manager")
+
+    suspend fun saveLayoutToPreferencesDataStore(isLinearLayoutManager: Boolean, context: Context) {
+        context.dataStore.edit { preferences ->
+            preferences[IS_LINEAR_LAYOUT_MANAGER] = isLinearLayoutManager
+        }
+    }
+
 }
 
 private const val LAYOUT_PREFERENCES_NAME = "layout_preferences"
