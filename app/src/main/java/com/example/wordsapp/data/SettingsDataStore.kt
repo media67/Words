132532dbe3a0c5e95b7506c2966068897f869c5f
Samuel Bourassa <district67@gmail.com>
2023-03-09 10:22:46 -0500

    Expose a preferenceFlow: Flow<UserPreferences>, constructed base on dataStore.data: Flow<Preferences>.

--- app/src/main/java/com/example/wordsapp/data/SettingsDataStore.kt
+++ app/src/main/java/com/example/wordsapp/data/SettingsDataStore.kt
@@ -5,8 +5,12 @@ import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.emptyPreferences
 import androidx.datastore.preferences.preferencesDataStore
-import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import java.io.IOException
 
 
 class SettingsDataStore(context: Context) {
@@ -17,6 +21,20 @@ class SettingsDataStore(context: Context) {
             preferences[IS_LINEAR_LAYOUT_MANAGER] = isLinearLayoutManager
         }
     }
+
+    val preferenceFlow: Flow<Boolean> = context.dataStore.data
+        .catch {
+            if (it is IOException) {
+                it.printStackTrace()
+                emit(emptyPreferences())
+            } else {
+                throw it
+            }
+        }
+        .map { preferences ->
+            // One the first run of the app, we will use LinearLayoutManager by default
+            preferences[IS_LINEAR_LAYOUT_MANAGER] ?: true
+        }
 
 }
 
